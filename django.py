2f7a5b79b8df76371a6a2a762578ef533100b10d
shavendra <shavendragoesoft@gmail.com>
2022-02-22 16:16:21 +0530

    add new model

--- django.py
+++ django.py
@@ -1,23 +1,10 @@
-# send object with template
-#################################################
-############  view page #########################
-#################################################
-
-from django.shortcuts import render
-from django.http import HttpResponse
-import numpy as np
-
-# Create your views here.
+from django.db import models
 
 
+class Project(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=255, null=False)
+    image = models.ImageField(upload_to='portfolio/images/')
 
-def formpost(request):
-    length_of_post = request.POST['length']  # one way to doing it 
-    value = "".join([ str(np.random.random_integers(10)) for _ in range(int(length_of_post))])
-    print(value)
-    return render(request, 'generator/password.html',{'gen':value})
-
-
-
-# or use this thing 
-     length_of_post = request.POST.get('length')
+    def __str__(self):
+        return f"{self.title} {self.description}"
