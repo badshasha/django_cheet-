2f16e27aca90e21941c7b74d95f3abf713441421
shavendra <shavendragoesoft@gmail.com>
2022-02-22 11:29:39 +0530

    how to reaed form information

--- django.py
+++ django.py
@@ -5,11 +5,19 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+import numpy as np
 
 # Create your views here.
 
 
 
+def formpost(request):
+    length_of_post = request.POST['length']  # one way to doing it 
+    value = "".join([ str(np.random.random_integers(10)) for _ in range(int(length_of_post))])
+    print(value)
+    return render(request, 'generator/password.html',{'gen':value})
 
-def homepageNew(request):
-    return render(request,'appname/templatge.html' , {'key' :1234})        # {{ key }}        [access using this ]                     
+
+
+# or use this thing 
+     length_of_post = request.POST.get('length')
