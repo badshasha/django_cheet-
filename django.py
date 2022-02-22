d9d867ccc5a59aad6ab1bca086ba22baf0cd2827
shavendra <shavendragoesoft@gmail.com>
2022-02-22 16:43:46 +0530

    how to watch images

--- django.py
+++ django.py
@@ -1,4 +1,14 @@
-# add meaid to root directory 
-# setting file 
+# watch image on site 
 
+# setting file 
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
+# url maning page
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
