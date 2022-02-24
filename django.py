3d83228b914a19fb39421c6aecf288ad4fdeaf74
shavendra <62420437+badshasha@users.noreply.github.com>
2022-02-24 12:18:35 +0530

    how to send route to information view

--- django.py
+++ django.py
@@ -1,1 +1,17 @@
 
+# path with route id 
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.gloghome,name='blogHomePage'),
+    path('<int:blog_id>/' , views.selectedBlogPage , name='selectePage'),
+
+]
+
+
+# views page 
+
+def selectedBlogPage(request,blog_id):
+    return render(request , 'blog/selected.html')
