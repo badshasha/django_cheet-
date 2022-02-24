dd13effce35231cb61ad79acc70678d5a6dbb9fa
shavendra <shavendragoesoft@gmail.com>
2022-02-24 21:26:57 +0530

    delete content

--- django.py
+++ django.py
@@ -1,16 +1,8 @@
+# delete todo
 
-def todopageinfo(request,todo_id):
-    todo = get_object_or_404(Todo , pk=todo_id , user= request.user) # other wise user can access to all other objects 
-
-    if request.method == "GET":
-        form = TodoForms(instance=todo)
-        return render(request , 'todo/todoinfo.html' , { 'todo' : todo , 'form' : form })
-    else:
-        try:
-            form = TodoForms(request.POST,instance=todo)  # most important part [instance todo important]
-            form.save()
-            print("working")
-            return redirect('todo:todoall')
-        except ValueError:
-            return HttpResponse("bad information ")
-
+@login_required
+def tododelete(request,todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id, user=request.user)  # safe content raking
+    if request.method == "POST":
+        todo.delete()
+        return redirect('todo:todoall')
