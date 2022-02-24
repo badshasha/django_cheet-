bf4422691c118714a7407b50b384e31bb4e31938
shavendra <shavendragoesoft@gmail.com>
2022-02-24 16:31:31 +0530

    update content

--- django.py
+++ django.py
@@ -1,6 +1,16 @@
-# form with all fillded informatnio 
 
 def todopageinfo(request,todo_id):
-    todo = get_object_or_404(Todo , pk=todo_id)  # find the instance 
-    form = TodoForms(instance=todo)              # create form 
-    return render(request , 'todo/todoinfo.html' , { 'todo' : todo , 'form' : form })
+    todo = get_object_or_404(Todo , pk=todo_id)
+
+    if request.method == "GET":
+        form = TodoForms(instance=todo)
+        return render(request , 'todo/todoinfo.html' , { 'todo' : todo , 'form' : form })
+    else:
+        try:
+            form = TodoForms(request.POST,instance=todo)  # most important part [instance todo important]
+            form.save()
+            print("working")
+            return redirect('todo:todoall')
+        except ValueError:
+            return HttpResponse("bad information ")
+
