bc86418f87de74d8a43cf24a7d676310704b8bca
shavendra <shavendragoesoft@gmail.com>
2022-02-24 16:17:41 +0530

    little change [update form]

--- django.py
+++ django.py
@@ -1,30 +1,6 @@
-# add form to database 
+# form with all fillded informatnio 
 
-
-# there are two ways to do it 
-    # using form   --> form .save
-    # using create new model  -> model.save()
-
-
-# first method 
-def todocreate(request):
-    error = ""
-    if request.method == "POST":
-        if request.POST["title"] is not None and request.POST["title"] != "":
-            form = TodoForms(request.POST)
-            newtodo = form.save(commit=False) # dont save it on the database
-            newtodo.user = request.user       # add user 
-            newtodo.save()
-            return redirect('todo:todoall')
-        else:
-            error = "empty title not allow"
-    return render(request, 'todo/createTodo.html' , {'form' : TodoForms() ,'error' : error} )
-
-
-# show specific user 
-
-
-def todoContent(request):
-    todos_not = Todo.objects.filter(user=request.user, datecompleted__isnull=True)  # read not compleated
-    todos_compleate = Todo.objects.filter(user=request.user, datecompleted__isnull=False) # read compleated
-    return render(request, 'todo/allinfo.html' , {'todos' : todos })
+def todopageinfo(request,todo_id):
+    todo = get_object_or_404(Todo , pk=todo_id)  # find the instance 
+    form = TodoForms(instance=todo)              # create form 
+    return render(request , 'todo/todoinfo.html' , { 'todo' : todo , 'form' : form })
