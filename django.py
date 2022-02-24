9afb8c0bd45843dae2893d384f9908e30fae3798
shavendra <62420437+badshasha@users.noreply.github.com>
2022-02-24 08:26:10 +0530

    login

--- django.py
+++ django.py
@@ -7,3 +7,25 @@ from django.contrib.auth.forms import  UserCreationForm
 from  django.contrib.auth import login , logout
 from django.db import IntegrityError
 
+# how to log in 
+
+def todopage(request):
+    error = None
+    if request.method == "POST":
+        if request.POST["password1"] == request.POST["password2"]:
+            if request.POST["username"]:
+                try:
+                    user = User.objects.create_user(request.POST["username"], email=None, password=request.POST["password1"])  # new user create 
+                    user.save()  # user save in the database
+                    
+                    login(request,user)  # log in function 
+                    
+                    return redirect('todo:todoall') # redirect 
+                except IntegrityError:
+                    error = "user name not available [please try different user name]"
+            else:
+                error = "empty user name not allowed"
+        else:
+            error = "password and confirm password are not matching"
+
+    return render(request, 'todo/todoHomepage.html', {'form': UserCreationForm(), 'error': error})
